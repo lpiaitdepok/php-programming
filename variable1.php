de9b6b8e3ade0f69a328289a759e5e497ac4c196
lpiaitdepok <lpiaitdepok@gmail.com>
2021-03-24 10:38:55 +0700

    Create variable1.php

---
+++ variable1.php
@@ -0,0 +1,12 @@
+//Syntax of declaring a variable in PHP is given below:
+
+//$variablename=value;
+
+<?php  
+$str="hello string";  
+$x=200;  
+$y=44.6;  
+echo "string is: $str <br/>";  
+echo "integer is: $x <br/>";  
+echo "float is: $y <br/>";  
+?> 
