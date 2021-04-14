7b462f6c10c7ae54c4b37986b1930dbf11906398
lpiaitdepok <lpiaitdepok@gmail.com>
2021-04-14 16:40:18 +0700

    Update config.php

--- crud1/config.php
+++ crud1/config.php
@@ -1,6 +1,6 @@
 <?php
 //koneksi ke database mysql, silahkan di rubah dengan koneksi database sendiri
-$koneksi = mysqli_connect("localhost","root","","tutorialweb_crud");
+$koneksi = mysqli_connect("localhost","root","","databasename");
 
 //cek jika koneksi ke mysql gagal, maka akan tampil pesan berikut
 if (mysqli_connect_errno()){
