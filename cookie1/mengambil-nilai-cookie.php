cca6ac962cafdd7a9f101335aaf7a939a62ecfad
lpiaitdepok <lpiaitdepok@gmail.com>
2021-04-28 17:04:08 +0700

    Create mengambil-nilai-cookie.php

---
+++ cookie1/mengambil-nilai-cookie.php
@@ -0,0 +1,18 @@
+<?php
+$nama_cookie = "pengguna";
+$nilai_cookie = "Tutorial Web";
+setcookie($nama_cookie, $nilai_cookie, time() + (86400 * 30), "/"); // 86400 	= 1 hari
+?>
+<html>
+<body>
+ 
+<?php
+if(!isset($_COOKIE[$nama_cookie])) {
+    echo "Nama Cookie'" . $nama_cookie . "' belum dibuat!";
+} else {
+    echo "Cookie '" . $nama_cookie . "' sudah dibuat!<br>";
+    echo "Nilai Cookie adalah : " . $_COOKIE[$nama_cookie];
+}
+?>
+</body>
+</html>
