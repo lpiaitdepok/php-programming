33a1e9969f257fff56cface996ae6d34eb33ab9b
lpiaitdepok <lpiaitdepok@gmail.com>
2021-05-03 16:48:49 +0700

    Update provinsi.sql

--- database/Menampilkan Dropdown Select Option Dari Database MySQL PHP/provinsi.sql
+++ database/Menampilkan Dropdown Select Option Dari Database MySQL PHP/provinsi.sql
@@ -4,3 +4,9 @@ CREATE TABLE `provinsi` (
 `id_provinsi` int(11) NOT NULL PRIMARY KEY  AUTO_INCREMENT,
 `provinsi` varchar(255)
 );
+INSERT INTO `provinsi` VALUES ('1', 'ACEH');
+INSERT INTO `provinsi` VALUES ('2', 'SUMATERA UTARA');
+INSERT INTO `provinsi` VALUES ('3', 'SUMATERA BARAT');
+INSERT INTO `provinsi` VALUES ('4', 'RIAU');
+INSERT INTO `provinsi` VALUES ('5', 'JAMBI');
+INSERT INTO `provinsi` VALUES ('6', 'BENGKULU');
