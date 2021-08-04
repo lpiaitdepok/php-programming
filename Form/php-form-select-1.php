a07dd158a75a2eafed88ae05eee16c7a9a1c08ce
lpiaitdepok <lpiaitdepok@gmail.com>
2021-08-04 19:27:04 +0700

    Update php-form-select-1.php

--- Form/php-form-select-1.php
+++ Form/php-form-select-1.php
@@ -1,10 +1,23 @@
+<?php
+$gender = '';
+if (!empty($_POST)) {
+    $gender = $_POST['formGender'];
+}
+if (!empty($_GET)) {
+    $gender = $_GET['formGender'];
+}
+?>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 <p>
 What is your Gender?
 <select name="formGender">
   <option value="">Select...</option>
-  <option value="M" <?php echo $_POST['formGender']=='M' ? 'selected' : '' ?>>Male</option>
-  <option value="F" <?php echo $_POST['formGender']=='F? 'selected' : '' ?>>Female</option>
+  <option value="M" <?php echo $gender=='M' ? 'selected' : '' ?>>Male</option>
+  <option value="F" <?php echo $gender=='F' ? 'selected' : '' ?>>Female</option>
 </select>
+</p>
+<p>
+<input type="submit" value="">
+<button type="submit">Submit</button>
 </p>
 </form>
